677bb52007a7089529e74729d83a58def424c001
c4fiber <qudcjf153@gmail.com>
2023-11-21 13:12:31 +0900

    feat: 하단 네이게이션 바에 myroom 매핑

--- lib/bottombar.dart
+++ lib/bottombar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'groupMain.dart';
+import 'package:iww_frontend/screens/myroom.dart';
 
 class MyBottomNav extends StatelessWidget implements PreferredSizeWidget {
   const MyBottomNav({super.key});
@@ -11,11 +12,18 @@ class MyBottomNav extends StatelessWidget implements PreferredSizeWidget {
 // TODO : 다른 페이지 라우팅은 여기서!
 // 이미 home인디 Home 으로 이동하는 경우는 아무것도 하지않도록 하는 코드 추가 필요
   void goHome(context, uri) {
-    if (uri == 0) {
-      Navigator.push(context, MaterialPageRoute(builder: (c) => MyHome()));
-    }
-    if (uri == 1) {
-      Navigator.push(context, MaterialPageRoute(builder: (c) => MyGroup()));
+    switch(uri) {
+      case 0:
+        Navigator.push(context, MaterialPageRoute(builder: (c) => MyHome()));
+        break;
+      case 1:
+        Navigator.push(context, MaterialPageRoute(builder: (c) => MyGroup()));
+        break;
+      case 2:
+        Navigator.push(context, MaterialPageRoute(builder: (c) => MyRoom()));
+        break;
+      default:
+        throw UnimplementedError('no widget for uri $uri');
     }
   }
 
@@ -30,7 +38,7 @@ class MyBottomNav extends StatelessWidget implements PreferredSizeWidget {
         onTap: (i) {
           goHome(context, i);
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.check_box_outlined), label: "할일"),
           BottomNavigationBarItem(
