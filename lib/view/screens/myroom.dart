65ac18f95a7b41a9bedb1abbc5f14dfc6847122a
c4fiber <qudcjf153@gmail.com>
2023-11-23 03:19:40 +0900

    feat: myroom에 hp, exp 추가

--- lib/view/screens/myroom.dart
+++ lib/view/screens/myroom.dart
@@ -6,6 +6,7 @@ import 'package:iww_frontend/view/widget/guestbook.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 import 'package:iww_frontend/view/widget/bottombar.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 
 class MyRoom extends StatelessWidget {
   const MyRoom({super.key});
@@ -67,7 +68,8 @@ class RenderMyRoom extends StatelessWidget {
       disableZoom: true,
       cameraControls: false,
       // animationName: "walk",
-      cameraOrbit: "40deg 60eg 0m", // theta, phi, radius
+      cameraOrbit: "40deg 60eg 0m",
+      // theta, phi, radius
       cameraTarget: "0.5m 1.5m 2m", // x(왼쪽 위), y(높이) ,z (오른쪽 위)
     ),
     'robot': ModelViewer(
@@ -80,7 +82,8 @@ class RenderMyRoom extends StatelessWidget {
       disableZoom: true,
       cameraControls: false,
       animationName: "walk",
-      cameraOrbit: "30deg 60deg 0m", // theta, phi, radius
+      cameraOrbit: "30deg 60deg 0m",
+      // theta, phi, radius
       cameraTarget: "1m 4m 4m", // x(왼쪽 위), y(높이) ,z (오른쪽 위)
     ),
     'animals': ModelViewer(
@@ -93,7 +96,8 @@ class RenderMyRoom extends StatelessWidget {
       disableZoom: true,
       // cameraControls: false,
       // animationName: "walk",
-      cameraOrbit: "30deg 30deg 2m", // theta, phi, radius
+      cameraOrbit: "30deg 30deg 2m",
+      // theta, phi, radius
       cameraTarget: "2m 2m 2m", // x(왼쪽 위), y(높이) ,z (오른쪽 위)
     ),
     'cat': ModelViewer(
@@ -121,7 +125,9 @@ class RenderMyRoom extends StatelessWidget {
     final authService = Provider.of<AuthService>(context);
     final commentsProvider = context.read<CommentsProvider>();
 
-    // set
+    var HP = 0.5;
+
+    // get argument from navigator, context
     try {
       _isMyRoom = ModalRoute.of(context)!.settings.arguments as bool;
     } catch (e) {
@@ -144,47 +150,55 @@ class RenderMyRoom extends StatelessWidget {
     // bottom buttons
     layers.children.add(Positioned(
       bottom: 0,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      height: 240,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
         children: [
-          ElevatedButton(
-              onPressed: () async {
-                String? roomOwenerId = commentsProvider.roomOwnerId;
+          Flexible(flex: 1, child: StatusBar()),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    String? roomOwenerId = commentsProvider.roomOwnerId;
 
-                final currentUser = await authService.getCurrentUser();
-                // 로그인 유저 없으면 6
-                var userId = (currentUser != null)
-                    ? currentUser.user_id.toString()
-                    : '6';
+                    final currentUser = await authService.getCurrentUser();
+                    // 로그인 유저 없으면 6
+                    var userId = (currentUser != null)
+                        ? currentUser.user_id.toString()
+                        : '6';
 
-                if (context.mounted) {
-                  showCommentsBottomSheet(
-                    context,
-                    commentsProvider,
-                    userId,
-                    roomOwenerId,
-                  );
-                }
-              },
-              child: Text('방명록')),
-          SizedBox(width: 20),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/inventory');
-              },
-              child: Text('인벤토리')),
-          SizedBox(width: 20),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    "/myroom",
-                    arguments: !_isMyRoom,
-                    (route) => false);
-              },
-              child: Text('친구목록')),
+                    if (context.mounted) {
+                      showCommentsBottomSheet(
+                        context,
+                        commentsProvider,
+                        userId,
+                        roomOwenerId,
+                      );
+                    }
+                  },
+                  child: Text('방명록')),
+              SizedBox(width: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/inventory');
+                  },
+                  child: Text('인벤토리')),
+              SizedBox(width: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        "/myroom",
+                        arguments: !_isMyRoom,
+                        (route) => false);
+                  },
+                  child: Text('친구목록')),
+            ],
+          ),
+          SizedBox(height: 10),
         ],
       ),
     ));
@@ -226,3 +240,84 @@ class RenderMyRoom extends StatelessWidget {
 //     );
 //   }
 // }
+
+class StatusBar extends StatefulWidget {
+  const StatusBar({super.key});
+
+  @override
+  State<StatusBar> createState() => _StatusBarState();
+}
+
+class _StatusBarState extends State<StatusBar> with TickerProviderStateMixin {
+  Timer? _timer;
+  var _hp = 0.3;
+  var _exp = 0.1;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      // Call _setHP method here to update the hp value every 5 seconds
+      setState(() {
+        _hp >= 1 ? _hp : _hp += 0.1;
+        _exp >= 1 ? _exp : _exp += 0.2;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer!.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              children: const [
+                SizedBox(width: 10),
+                Text(
+                  'HP',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
+                )
+              ],
+            ),
+            LinearProgressIndicator(
+              value: _hp,
+              minHeight: 14,
+              valueColor: AlwaysStoppedAnimation<Color>(
+                  const Color.fromARGB(255, 239, 118, 110)),
+              backgroundColor: Colors.grey[200],
+              semanticsLabel: 'Linear progress indicator',
+            ),
+            Row(
+              children: const [
+                SizedBox(width: 10),
+                Text(
+                  'EXP',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
+                )
+              ],
+            ),
+            LinearProgressIndicator(
+              value: _exp,
+              minHeight: 14,
+              valueColor: AlwaysStoppedAnimation<Color>(
+                  Color.fromARGB(255, 155, 239, 110)),
+              backgroundColor: Colors.grey[200],
+              semanticsLabel: 'Linear progress indicator',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
