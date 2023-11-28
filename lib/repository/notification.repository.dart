c42f7a4d654e8eb9132aa2297635abe96482af3b
coding-jjun <psjbrian@gmail.com>
2023-11-28 21:44:39 +0900

    feat: notification history

--- lib/repository/notification.repository.dart
+++ lib/repository/notification.repository.dart
@@ -1,26 +1,89 @@
 import 'dart:convert';
-
 import 'package:iww_frontend/datasource/remoteDataSource.dart';
 import 'package:iww_frontend/model/notification/notification.model.dart';
 
 class NotificationRepository {
-  Future<List<Notification>?> getNoti(int? userId) async {
-    return await RemoteDataSource.get("/notification").then((response) {
-      if (response.statusCode == 200) {
-        List<dynamic> jsonData = jsonDecode(response.body);
+  List<Notification> dummy = [
+    Notification(
+        notiId: '6',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '2',
+        senderName: '병철',
+        notiType: 4,
+        reqType: 0,
+        subId: 0,
+        todoTitle: ''),
+    Notification(
+        notiId: '5',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '3',
+        senderName: '소정',
+        notiType: 3,
+        reqType: 0,
+        subId: 0,
+        todoTitle: '알림 만들기'),
+    Notification(
+        notiId: '4',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '2',
+        senderName: '병철',
+        notiType: 2,
+        reqType: 0,
+        subId: 0,
+        todoTitle: '미라클 모닝하기'),
+    Notification(
+        notiId: '3',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '4',
+        senderName: '다희',
+        notiType: 1,
+        reqType: 0,
+        subId: 0,
+        todoTitle: ''),
+    Notification(
+        notiId: '2',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '3',
+        senderName: '소정',
+        notiType: 0,
+        reqType: 0,
+        subId: 0,
+        todoTitle: ''),
+    Notification(
+        notiId: '1',
+        receiverId: '1',
+        receiverName: '세준',
+        senderId: '2',
+        senderName: '병철',
+        notiType: 0,
+        reqType: 1,
+        subId: 0,
+        todoTitle: ''),
+  ];
 
-        if (jsonData.isEmpty) {
-          return null;
-        }
+  Future<List<Notification>> getNoti(int? userId) async {
+    // return await RemoteDataSource.get("/notification").then((response) {
+    //   if (response.statusCode == 200) {
+    //     List<dynamic> jsonData = jsonDecode(response.body);
 
-        List<Notification>? data =
-            jsonData.map((data) => Notification.fromJson(data)).toList();
+    //     if (jsonData.isEmpty) {
+    //       return [];
+    //     }
 
-        return data;
-      }
+    //     List<Notification>? data =
+    //         jsonData.map((data) => Notification.fromJson(data)).toList();
 
-      return null;
-    });
+    //     return data;
+    //   }
+
+    //   return [];
+    // });
+    return dummy;
   }
 
   Future<bool> createNoti(int? receiverId, Map<String, dynamic> data) async {
--- lib/view/notification/notification.dart
+++ lib/view/notification/notification.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:iww_frontend/repository/notification.repository.dart';
+import 'package:iww_frontend/secrets/secrets.dart';
 import 'package:iww_frontend/service/auth.service.dart';
 import 'package:iww_frontend/view/_common/appbar.dart';
+import 'package:iww_frontend/model/notification/notification.model.dart'
+    as model;
 
 class MyNotification extends StatefulWidget {
   const MyNotification({super.key});
@@ -11,7 +14,7 @@ class MyNotification extends StatefulWidget {
 }
 
 class _MyNotificationState extends State<MyNotification> {
-  List<Notification> notifications = [];
+  List<model.Notification> notifications = [];
   final NotificationRepository notificationRepository =
       NotificationRepository();
 
@@ -22,25 +25,117 @@ class _MyNotificationState extends State<MyNotification> {
   }
 
   Future<void> fetchNoti() async {
-    List<Notification> fectedNoti = await notificationRepository.getNoti(userId);
+    List<model.Notification> fectedNoti =
+        //TODO - userId 받아오는 코드 추가
+        await notificationRepository.getNoti(1);
+    notifications = fectedNoti;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(),
-      body: ListView.builder(
-        itemCount: notifications.length,
-        itemBuilder:(context, index) {
-          var noti = notifications[index];
-          return ListTile (
-            title: Text(buildNotiMessage(noti)),
-            trailing: buildTrailWidget(noti),
-            onTap: () => navigateToSender(noti),
-          );
-        },
-      )
-    );
+        appBar: MyAppBar(),
+        body: ListView.builder(
+          itemCount: notifications.length,
+          itemBuilder: (context, index) {
+            var noti = notifications[index];
+            return ListTile(
+              leading: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: NetworkImage(
+                          '${Secrets.REMOTE_SERVER_URL}/image/${noti.senderId}.jpg'),
+                      onBackgroundImageError: (exception, stackTrace) {},
+                      child: Image.network(
+                        '${Secrets.REMOTE_SERVER_URL}/image/${noti.senderId}.jpg',
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return CircleAvatar(
+                            radius: 20,
+                            backgroundImage: AssetImage('assets/profile.jpg'),
+                          );
+                        },
+                      ))),
+              title: Text(buildNotiMessage(noti)),
+              trailing: buildTrailWidget(noti),
+              onTap: () => navigateToSender(noti),
+            );
+          },
+        ));
+  }
 
-  String 
+  String buildNotiMessage(model.Notification noti) {
+    var sender = noti.senderName;
+    String message;
+    switch (noti.notiType) {
+      case 0:
+        message = '$sender님이 친구 추가 요청을 보냈습니다.';
+        break;
+      case 1:
+        message = '$sender님이 친구 추가 요청을 수락하였습니다.';
+        break;
+      case 2:
+        message = '$sender님이 "${noti.todoTitle}"에 대한 인증을 요청하였습니다.';
+        break;
+      case 3:
+        message = '$sender님이 "${noti.todoTitle}"에 대한 인증을 완료하였습니다.';
+        break;
+      case 4:
+        message = '$sender님이 방명록에 댓글을 남겼습니다.';
+        break;
+      default:
+        message = '';
+    }
+    return message;
+  }
+
+  Widget? buildTrailWidget(model.Notification noti) {
+    if (noti.notiType == 0 && noti.reqType == 0) {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+              onPressed: () async {
+                noti.reqType = 1;
+                await notificationRepository.updateNoti(
+                    noti.notiId, noti.toJson());
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: Text('수락')),
+          SizedBox(width: 8),
+          ElevatedButton(
+            onPressed: () async {
+              noti.reqType = 2;
+              await notificationRepository.updateNoti(
+                  noti.notiId, noti.toJson());
+            },
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            child: Text('거절'),
+          ),
+        ],
+      );
+    } else if (noti.notiType == 2) {
+      return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        child: Text('확인하기'),
+      );
+    }
+    return null;
+  }
+
+  void navigateToSender(model.Notification noti) {
+    switch (noti.notiType) {
+      case 0 || 1:
+        // TODO - sender의 myroom으로 redirect
+        break;
+      case 2 || 3:
+        // TODO - 해당 todo의 인증샷에 대한 modal 창으로 redirect
+        break;
+      case 4:
+        // TODO - 내 방명록의 해당 댓글 위치로 redirect
+        break;
+    }
+  }
 }
