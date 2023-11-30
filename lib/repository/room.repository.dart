e7beb3a62aea7b91b7e2d641850ead6165208e73
c4fiber <qudcjf153@gmail.com>
2023-11-30 19:36:19 +0900

    fix: room repository 오류 수정

--- lib/repository/room.repository.dart
+++ lib/repository/room.repository.dart
@@ -2,14 +2,16 @@ import 'package:iww_frontend/datasource/remoteDataSource.dart';
 import 'package:iww_frontend/model/item/item.model.dart';
 import 'package:iww_frontend/utils/logger.dart';
 
+class RoomRepository {
 // 특정 유저의 룸 정보 가져오기
-Future<List<Item>> getItemsOfMyRoom(int userId) async {
-  return await RemoteDataSource.get('/room/$userId').then((response) {
-    List<Item> items =
-        (response.body as List).map((item) => Item.fromJson(item)).toList();
-    LOG.log('Get room: ${response.statusCode}');
-    LOG.log('items: ${response.body}');
+  Future<List<Item>> getItemsOfMyRoom(int userId) async {
+    return await RemoteDataSource.get('/room/$userId').then((response) {
+      List<Item> items =
+          (response.body as List).map((item) => Item.fromJson(item)).toList();
+      LOG.log('Get room: ${response.statusCode}');
+      LOG.log('items: ${response.body}');
 
-    return items;
-  });
+      return items;
+    });
+  }
 }
