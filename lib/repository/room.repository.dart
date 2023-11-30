5655ba31e52f02dd54ab19ded2d16d6fd6045ed3
c4fiber <qudcjf153@gmail.com>
2023-11-30 21:09:21 +0900

    fix: fetch items of my room 수정

--- lib/repository/room.repository.dart
+++ lib/repository/room.repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:iww_frontend/datasource/remoteDataSource.dart';
 import 'package:iww_frontend/model/item/item.model.dart';
 import 'package:iww_frontend/utils/logger.dart';
@@ -6,8 +8,9 @@ class RoomRepository {
 // 특정 유저의 룸 정보 가져오기
   Future<List<Item>> getItemsOfMyRoom(int userId) async {
     return await RemoteDataSource.get('/room/$userId').then((response) {
+      Map<String, dynamic> parsedJson = json.decode(response.body);
       List<Item> items =
-          (response.body as List).map((item) => Item.fromJson(item)).toList();
+          (parsedJson['result'] as List<dynamic>).map((item) => Item.fromJson(item)).toList();
       LOG.log('Get room: ${response.statusCode}');
       LOG.log('items: ${response.body}');
 
