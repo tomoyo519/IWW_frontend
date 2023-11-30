0b0a4748fc34ac96e27e8754b41bcb8de8f62ecd
c4fiber <qudcjf153@gmail.com>
2023-12-01 01:28:40 +0900

    fix: 인벤토리 요청 url 수정

--- lib/repository/room.repository.dart
+++ lib/repository/room.repository.dart
@@ -7,7 +7,7 @@ import 'package:iww_frontend/utils/logger.dart';
 class RoomRepository {
 // 특정 유저의 룸 정보 가져오기
   Future<List<Item>> getItemsOfMyRoom(int userId) async {
-    return await RemoteDataSource.get('/room/$userId').then((response) {
+    return await RemoteDataSource.get('/item-inventory/$userId').then((response) {
       Map<String, dynamic> parsedJson = json.decode(response.body);
       List<Item> items =
           (parsedJson['result'] as List<dynamic>).map((item) => Item.fromJson(item)).toList();
