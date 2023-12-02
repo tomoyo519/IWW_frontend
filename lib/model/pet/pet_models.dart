7c555da0d3c8bd3cc9ab937b8989ae5604904583
c4fiber <qudcjf153@gmail.com>
2023-12-02 23:06:47 +0900

    refactor: 모델 이름변경 -> 구미호01, 02, 03

--- lib/model/pet/pet_models.dart
+++ lib/model/pet/pet_models.dart
@@ -16,7 +16,7 @@ class PetModels {
   // fieldOfView: "120deg",
   // 2 2 0 (좀 작은 버전)
   static final Map<String, ModelViewer> _pets = {
-    'kitsune': ModelViewer(
+    '구미호03': ModelViewer(
       src: 'assets/pets/kitsune.glb',
       interactionPrompt: InteractionPrompt.none,
       autoPlay: true,
@@ -30,7 +30,22 @@ class PetModels {
       autoRotateDelay: 0,
       rotationPerSecond: "0.6rad",
     ),
-    'small_fox': ModelViewer(
+    '구미호02': ModelViewer(
+      src: 'assets/pets/mid_fox.glb',
+      interactionPrompt: InteractionPrompt.none,
+      autoPlay: true,
+      autoRotate: true,
+      shadowIntensity: 1,
+      disableZoom: true,
+      cameraControls: false,
+      animationName: "Walk",
+      cameraTarget: "0.7m 0.7m 0m", // x, y, z
+      cameraOrbit: "0deg 70deg 5m",
+      // fieldOfView: "90deg",
+      autoRotateDelay: 0,
+      rotationPerSecond: "0.7rad",
+    ),
+    '구미호01': ModelViewer(
       src: 'assets/pets/small_fox.glb',
       interactionPrompt: InteractionPrompt.none,
       autoPlay: true,
@@ -45,17 +60,9 @@ class PetModels {
       autoRotateDelay: 0,
       rotationPerSecond: "0.7rad",
     ),
-    'mid_fox': ModelViewer(
-      interactionPrompt: InteractionPrompt.none,
-      autoPlay: true,
-      autoRotate: true,
-      shadowIntensity: 1,
-      disableZoom: true,
-      cameraControls: false,
-      src: 'assets/pets/mid_fox.glb',
-      animationName: "Idle_A",
-      cameraOrbit: "30deg, 150deg, 0m",
-      cameraTarget: "0m 0.8m 0.4m",
-    ),
+ 
+
+
+ 
   };
 }
