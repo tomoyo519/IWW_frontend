96a9c219e02458bb49136c22ac2436110cb7dec9
cece <sojeong.lee017@gmail.com>
2023-12-11 15:27:48 +0900

    fix: [IWW-197][IWW-210][IWW-203] todo progressbar

--- lib/view/todo/todo_progress.dart
+++ lib/view/todo/todo_progress.dart
@@ -34,124 +34,95 @@ class _TodoProgressState extends State<TodoProgress> {
     double progress = todayTotal == 0 ? 0 : todayDone / todayTotal;
 
     return Container(
-        padding: EdgeInsets.all(2 * fs),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Color.fromARGB(255, 241, 241, 241),
-        ),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.all(2 * fs),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color.fromARGB(255, 241, 241, 241),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        today,
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 5 * fs,
-                          fontWeight: FontWeight.w800,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Text(
-                        "오늘의 할일은 무엇인가요?",
-                        style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 3.5 * fs,
-                          fontWeight: FontWeight.normal,
-                          fontFamily: AppTheme.FONT_FAMILY,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Container(
-                        width: 50 * fs,
-                        height: 18 * fs,
-                        margin: EdgeInsets.symmetric(vertical: 2 * fs),
-                        padding: EdgeInsets.symmetric(vertical: fs),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(2 * fs),
-                        ),
-                        child: Row(
-                          children: [
-                            _TodayBadge(
-                              title: "달성",
-                              data: model.todayDone,
-                              color: Colors.orange,
-                            ),
-                            _TodayBadge(
-                              title: "미달성",
-                              data: model.todayTotal - model.todayDone,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  Text(
+                    today,
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 5 * fs,
+                      fontWeight: FontWeight.w800,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                  SizedBox(
-                    width: 30 * fs,
-                    height: 35 * fs,
-                    child: ModelViewer(
-                      interactionPrompt: InteractionPrompt.none,
-                      src: 'assets/pets/$filepath.glb',
-                      animationName: 'Idle_A',
-                      autoPlay: true,
-                      cameraControls: false,
-                      cameraOrbit: '25deg 75deg 105%',
+                  Text(
+                    "오늘의 할일은 무엇인가요?",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 3.5 * fs,
+                      fontWeight: FontWeight.normal,
+                      fontFamily: AppTheme.FONT_FAMILY,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Container(
+                    width: 50 * fs,
+                    height: 18 * fs,
+                    margin: EdgeInsets.symmetric(vertical: 2 * fs),
+                    padding: EdgeInsets.symmetric(vertical: fs),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(2 * fs),
+                    ),
+                    child: Row(
+                      children: [
+                        _TodayBadge(
+                          title: "달성",
+                          data: model.todayDone,
+                          color: Colors.orange,
+                        ),
+                        _TodayBadge(
+                          title: "미달성",
+                          data: model.todayTotal - model.todayDone,
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-              Expanded(
-                  child: Row(
-                children: [
-                  // Text("달성률"),
-
-                  pi.LinearPercentIndicator(
-                    width: MediaQuery.of(context).size.width - 10 * fs,
-                    animation: true,
-                    lineHeight: 20.0,
-                    animationDuration: 2500,
-                    percent: progress,
-                    center: Text("달성률 $todayDone / $todayTotal"),
-                    barRadius: Radius.circular(10),
-                    progressColor: Colors.orange,
-                  ),
-                  // Container(
-                  //   margin: EdgeInsets.symmetric(vertical: fs),
-                  //   decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.circular(100),
-                  //     color: Colors.white,
-                  //   ),
-                  //   height: 3 * fs,
-                  //   // width: screen.width,
-                  //   child: FractionallySizedBox(
-                  //     alignment: Alignment.centerLeft,
-                  //     widthFactor: progress,
-                  //     child: Container(
-                  //       decoration: BoxDecoration(
-                  //         borderRadius: BorderRadius.circular(100),
-                  //         color: AppTheme.PRI_COLOR,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                ],
-              )),
-              // Row(children: [
-              //   Text(
-              //     "달성률",
-              //     style: Theme.of(context).textTheme.headlineLarge,
-              //   ),
-            ])
-        //   ],
-        // ),
-        );
+              SizedBox(
+                width: 30 * fs,
+                height: 35 * fs,
+                child: ModelViewer(
+                  interactionPrompt: InteractionPrompt.none,
+                  src: 'assets/pets/$filepath.glb',
+                  animationName: 'Idle_A',
+                  autoPlay: true,
+                  cameraControls: false,
+                  cameraOrbit: '25deg 75deg 105%',
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: pi.LinearPercentIndicator(
+              width: MediaQuery.of(context).size.width - 10 * fs,
+              animation: true,
+              lineHeight: 20.0,
+              animationDuration: 2500,
+              percent: progress,
+              center: Text("달성률 $todayDone / $todayTotal"),
+              barRadius: Radius.circular(10),
+              progressColor: Colors.orange,
+              backgroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
